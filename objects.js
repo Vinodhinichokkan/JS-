6bebfe66869bc02a1e7d099bd601ae4cad1c1ee9
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-09-20 13:21:53 +0530

    exe

---
+++ objects.js
@@ -0,0 +1,31 @@
+//objects
+
+const movie = {
+    actor: "vinodhini",
+    music: "hari",
+    director: "riyaan",
+    producer: "abi"
+};
+
+console.log(Object.keys (movie));
+
+//[ 'actor', 'music', 'director', 'producer' ]
+
+
+const cinema = {
+    actor: "vinodhini",
+    music: "hari",
+    director: "riyaan",
+    producer: "abi" 
+};
+
+console.log(Object.values (cinema));
+
+//[ 'vinodhini', 'hari', 'riyaan', 'abi' ]
+
+
+const channel ={
+
+
+
+}
